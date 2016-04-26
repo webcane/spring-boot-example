0a1ed92e3d9717611fd0f0238765598ac6a55b65
mniedre <webcane@ya.ru>
2016-04-26 16:47:08 +0300

    init

---
+++ boot-security/src/main/java/cane/brothers/spring/WebConfig.java
@@ -0,0 +1,18 @@
+package cane.brothers.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+	}
+	
+}
